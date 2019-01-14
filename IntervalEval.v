8f8f7030c2819fa9853a8a0fea95ae7f0036410e
erinshi <zhengshi@nat-oitwireless-inside-vapornet100-b-4371.Princeton.EDU>
2019-01-14 16:06:27 -0500

    recover IntervalEval.v

---
+++ IntervalEval.v
@@ -0,0 +1,24 @@
+Require Import Maps.
+Require Import Program.
+Require Import Interval.
+
+Fixpoint int_aeval (st : id -> interval) (a : aexp) : interval :=
+  match a with
+  | ANum n => IInterval (Some n) (Some n)
+  | AId x => st x
+  | APlus a1 a2 => Interval.add (int_aeval st a1) (int_aeval st a2)
+  | AMinus a1 a2  => Interval.add (int_aeval st a1) (Interval.neg (int_aeval st a2))
+  | AMult a1 a2 => Interval.mul (int_aeval st a1) (int_aeval st a2)
+  end.
+
+Lemma int_aeval_sound: forall (st : state) (m : total_map interval) (a : aexp),
+  (forall (x : id), include (m x) (st x)) ->
+  include (int_aeval m a) (aeval st a).
+Proof.
+intros. induction a.
+unfold int_aeval, aeval,include;split; apply BinInt.Z.le_refl.
+unfold int_aeval, aeval;auto.
+simpl; apply add_sound;auto.
+simpl; apply minus_sound;auto.
+simpl; apply mul_sound;auto.
+Qed.
